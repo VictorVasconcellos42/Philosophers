9dd2ee7224505ea650cbbf6ca839488c9c955535
vde-vasc <vde-vasc@student.42.rio>
2022-12-26 13:34:51 -0300

    First file created, again rs

--- philo/src/philosophers.c
+++ philo/src/philosophers.c
@@ -6,12 +6,23 @@
 /*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/26 13:16:56 by vde-vasc          #+#    #+#             */
-/*   Updated: 2022/12/26 13:17:55 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2022/12/26 13:27:24 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
+
+void	print_philo(char *str)
+
+{
+	printf("%s\n", str);
+}
+
 
 int	main(int argc, char **argv)
 
 {
+	if (argc == 2)
+		print_philo(argv[1]);
 	return (0);
 }
