36617593eea45d3400c4b1dbe137b69b2d80ccb3
vde-vasc <vde-vasc@student.42.rio>
2023-01-06 08:29:09 -0300

    add get time function

--- philo/src/philosophers.c
+++ philo/src/philosophers.c
@@ -6,23 +6,36 @@
 /*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/26 13:16:56 by vde-vasc          #+#    #+#             */
-/*   Updated: 2022/12/26 13:27:24 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2023/01/05 17:44:48 by vasconcel        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include "../include/philosophers.h"
 
-void	print_philo(char *str)
+long	get_time(void)
 
 {
-	printf("%s\n", str);
+	struct timeval	tp;
+	long		milliseconds;
+
+	gettimeofday(&tp, NULL);
+	milliseconds = tp.tv_sec * 1000;
+	milliseconds += tp.tv_usec / 1000;
+	return (milliseconds);
 }
 
 
-int	main(int argc, char **argv)
+int	main(void)
 
 {
-	if (argc == 2)
-		print_philo(argv[1]);
+	long start_time;
+
+	start_time = get_time();
+
+	while (1)
+	{
+		printf("%ld\n", get_time() - start_time);
+		usleep(200 * 1000);
+	}
 	return (0);
 }
