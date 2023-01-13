cb94abbdd760dd42f5b0e72cb44d995eac947cdd
vde-vasc <vde-vasc@student.42.rio>
2023-01-13 19:19:35 -0300

    Add error utils functions

---
+++ philo/src/error.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vde-vasc <vde-vasc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/13 07:45:16 by vde-vasc          #+#    #+#             */
+/*   Updated: 2023/01/13 19:14:19 by vde-vasc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/philosophers.h"
+
+int	error_philo(void)
+
+{
+	printf("Really? You need at least " \
+	"one philosopher for me to function.\n");
+	return (FALSE);
+}
+
+int	error_signal(int position)
+
+{
+	printf("Error! Argument [%i] is a negative number\n", position);
+	return (FALSE);
+}
+
+int	error_digit(int position)
+
+{
+	printf("Error! Argument [%i] is not a number\n", position);
+	return (FALSE);
+}
+
+int	error_format(void)
+
+{
+		printf("Error! ");
+		printf("Format: <Number_of_philosophers> <time_to_die> " \
+		"<time_to_eat> <time_to_sleep> " \
+		"[number_of_times_each_philosopher_must_eat]\n");
+		return (FALSE);	
+}
