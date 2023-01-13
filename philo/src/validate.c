853099e2f41bb294e8b708aaed4ad0d29829f898
vde-vasc <vde-vasc@student.42.rio>
2023-01-13 19:18:49 -0300

    Validate functions 60% created

--- philo/src/validate.c
+++ philo/src/validate.c
@@ -3,21 +3,58 @@
 /*                                                        :::      ::::::::   */
 /*   validate.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
+/*   By: vde-vasc <vde-vasc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 04:12:03 by vde-vasc          #+#    #+#             */
-/*   Updated: 2023/01/11 04:27:12 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2023/01/13 19:13:52 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philosophers.h"
 
+static int	ft_isdigit(char c)
+
+{
+	if (c >= '0' && c <= '9')
+		return (TRUE);
+	return (FALSE);
+}
+
+static int	str_isdigit(char *str)
+
+{
+	int i;
+
+	i = -1;
+	while (str[++i])
+	{
+		if (str[0] == '+')
+			i++;
+		if (!ft_isdigit(str[i]))
+			return (FALSE);
+	}
+	return (TRUE);
+}
+
 int	ft_valid_argc(int argc, char **argv)
 
 {
+	int i;
+
 	if (argc != 5 && argc != 6)
+		return (error_format());
+	else
 	{
-		printf("Error! %s\n", FORMAT);
-		return (NULL);
+		i = 0;
+		while (argv[++i])
+		{
+			if (argv[1][0] == '0')
+				return (error_philo());
+			if (str_isdigit(argv[i]) == FALSE)
+				if (argv[i][0] == '-')
+					return	(error_signal(i));
+				return (error_digit(i));
+		}
 	}
+	return (TRUE);
 }
