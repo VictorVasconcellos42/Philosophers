c39e7a9bc660362c8ca3132ac441ad963121bba3
vde-vasc <vde-vasc@student.42.rio>
2023-01-19 19:01:14 -0300

    Start fork ideal

--- philo/src/philosophers.c
+++ philo/src/philosophers.c
@@ -6,7 +6,7 @@
 /*   By: vde-vasc <vde-vasc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/26 13:16:56 by vde-vasc          #+#    #+#             */
-/*   Updated: 2023/01/18 12:58:42 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2023/01/19 16:14:03 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,14 @@ void	*routine(void *ph)
 	t_philo	*filo;
 
 	filo = (t_philo *)ph;
-	eat(filo);
-	dreams(filo);
-	think(filo);
+	check_menu(filo);
+	while (DINNER)
+	{
+		take_fork(filo);
+		eat(filo);
+		dreams(filo);
+		think(filo);
+	}
 	return (NULL);
 }
 
@@ -33,6 +38,7 @@ static void	start_philo(t_config *table, int t_philo)
 	while (++i < t_philo)
 	{
 		table->ph[i].index = i + 1;
+		table->ph[i].table = table; 	
 		pthread_create(&table->ph[i].philo, NULL, &routine, &table->ph[i]);
 	}
 }
@@ -56,6 +62,7 @@ int	main(int argc, char **argv)
 	init_vars(&table, argc, argv) == FALSE)
 		return (1);
 	table.ph = malloc(sizeof(t_philo) * table.number_of_philo);
+	init_fork(&table);
 	start_philo(&table, table.number_of_philo);
 	finish_philo(&table, table.number_of_philo);
 	return (0);
