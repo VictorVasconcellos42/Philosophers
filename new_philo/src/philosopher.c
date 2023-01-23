1de8c8da0a8f29cf13ebb1202f9b68455ee75383
vde-vasc <vde-vasc@student.42.rio>
2023-01-23 13:56:54 -0300

    Start new philo

--- new_philo/src/philosopher.c
+++ new_philo/src/philosopher.c
@@ -6,14 +6,18 @@
 /*   By: vde-vasc <vde-vasc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 10:57:27 by vde-vasc          #+#    #+#             */
-/*   Updated: 2023/01/23 10:58:55 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2023/01/23 12:08:54 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosophers.h"
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 
 {
+	t_table	table;
+
+	if (!ft_valid_argc(argc, argv) || !init_vars(&table, argc, argv))
+		return (0);
 	return (0);
 }
