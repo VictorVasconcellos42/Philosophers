64f7c9f514670316847aaca9b861b506ccfe3a75
vde-vasc <vde-vasc@student.42.rio>
2023-01-23 23:50:04 -0300

    change atoi to my atol

--- new_philo/src/init.c
+++ new_philo/src/init.c
@@ -6,7 +6,7 @@
 /*   By: vde-vasc <vde-vasc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 11:56:45 by vde-vasc          #+#    #+#             */
-/*   Updated: 2023/01/23 20:02:45 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2023/01/23 23:49:25 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,13 +81,13 @@ void	init_fork(t_table *table)
 int	init_vars(t_table *table, int argc, char **argv)
 
 {
-	table->n_philo = atoi(argv[1]);
-	table->time_die = atoi(argv[2]);
-	table->time_eat = atoi(argv[3]);
-	table->time_dream = atoi(argv[4]);
+	table->n_philo = ft_atol(argv[1]);
+	table->time_die = ft_atol(argv[2]);
+	table->time_eat = ft_atol(argv[3]);
+	table->time_dream = ft_atol(argv[4]);
 	table->start_clock = get_time();
 	if (argc == 6)
-		table->m_eat = atoi(argv[5]);
+		table->m_eat = ft_atol(argv[5]);
 	else
 		table->m_eat = -1;
 	if (!valid_times(table))
