41724012d86e23791b71638994aaa2bc8c14c366
vde-vasc <vde-vasc@student.42.rio>
2023-02-01 19:38:41 -0300

    Remove mutex unlock in main

--- philo/src/philosopher.c
+++ philo/src/philosopher.c
@@ -6,7 +6,7 @@
 /*   By: vde-vasc <vde-vasc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 10:57:27 by vde-vasc          #+#    #+#             */
-/*   Updated: 2023/01/28 15:31:39 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2023/02/01 18:30:57 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ int	main(int argc, char **argv)
 	pthread_mutex_init(&table.creed, NULL);
 	init_fork(&table);
 	start_philo(&table, table.n_philo);
-	pthread_mutex_unlock(&table.creed);
 	finish_philo(&table, table.n_philo);
 	return (0);
 }
